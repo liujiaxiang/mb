865f512169af52eed4ce8a950466b3fa931be9ea
jiazaima <401115780@qq.com>
2018-03-23 10:20:12 +0800

    更新 QuestionController.java

--- fcms/src/main/java/com/daymooc/fcms/question/QuestionController.java
+++ fcms/src/main/java/com/daymooc/fcms/question/QuestionController.java
@@ -135,10 +135,21 @@ public class QuestionController extends BaseController
 	//修改提问
 	public void mod()
 	{
-		int postId = getParaToInt(0);
-		Posts post = postService.getPostById(postId);
-		setAttr("aTypes", blogService.getArticleTypes());
-		setAttr("post", post);
-		render("mod_post.html");
+		if (getParaToInt(0) != null)
+		{
+			int userId = Db.queryInt("select userId from posts where id=?", getParaToInt(0));
+			if (getLoginAccountId() == userId)
+			{
+				int postId = getParaToInt(0);
+				Posts post = postService.getPostById(postId);
+				setAttr("aTypes", blogService.getArticleTypes());
+				setAttr("post", post);
+				render("mod_post.html");
+			}
+			else
+			{
+				renderError(404);
+			}
+		}	
 	}
 }
