0fed7e923cafa075bae89df831c5a4f6ee344d52
jiazaima <401115780@qq.com>
2018-03-30 11:28:33 +0800

    更新 VideoController.java

--- fcms/src/main/java/com/daymooc/fcms/video/VideoController.java
+++ fcms/src/main/java/com/daymooc/fcms/video/VideoController.java
@@ -24,6 +24,7 @@ import com.jfinal.aop.Clear;
 import com.jfinal.kit.Ret;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.upload.UploadFile;
+import com.jfinal.plugin.activerecord.Db;
 
 @Before({FrontAuthInterceptor.class})
 public class VideoController extends BaseController
